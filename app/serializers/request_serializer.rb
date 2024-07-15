1ee27eb3a28caf2f0aef35156ffa0850bc701932
James Okunlade <james.okunlade@gmail.com>
2024-07-15 01:05:47 +0100

    Dockerize app

--- app/serializers/request_serializer.rb
+++ app/serializers/request_serializer.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: requests
+#
+#  id           :bigint           not null, primary key
+#  amount_cents :bigint           not null
+#  description  :text
+#  status       :string           default("requested")
+#  title        :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  user_id      :bigint           not null
+#
+# Indexes
+#
+#  index_requests_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class RequestSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :amount_cents, :status
 
